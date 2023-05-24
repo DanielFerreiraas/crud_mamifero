8707082213599f17b2eeaf50492780268d39f138
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2023-05-24 03:10:49 -0300

    rota de post criada

--- dist/server.js
+++ dist/server.js
@@ -4,11 +4,41 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
+const cors_1 = __importDefault(require("@fastify/cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const client_1 = require("@prisma/client");
+dotenv_1.default.config();
+const prisma = new client_1.PrismaClient();
 const server = (0, fastify_1.default)();
+server.register(cors_1.default, {});
 server.get('/', (request, reply) => {
     return "Servidor Exemplo no ar";
 });
-server.listen({ port: 3000 }, (error, adress) => {
+server.post('/inserir', (request, reply) => {
+    const { nome, caracteristicas, peso, ameacado } = request.body;
+    const mamifero = prisma.leopardoDasNeves.create({
+        data: {
+            nome,
+            caracteristicas,
+            peso,
+            ameacado,
+        }
+    });
+    return reply.status(201).send(mamifero);
+});
+// server.get('/pesquisar', async (request: any, reply: any) => {
+//     const nomeDoMamifero = request.body.nomeDoMamifero;
+//     const leopardoDasNeves = await prisma.leopardoDasNeves.findMany({
+//         where:{
+//             nome: {
+//                 contains: nomeDoMamifero
+//             }
+//         }
+//     })
+//     reply.send(leopardoDasNeves)
+// })
+const port = process.env.PORT;
+server.listen({ port }, (error, adress) => {
     if (error) {
         console.error(error);
         process.exit(1);
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,49 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors'
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
+
+const prisma = new PrismaClient();
 const server = fastify();
 
 server.register(cors, {
-    
+
 });
 
 server.get('/', (request, reply) => {
     return "Servidor Exemplo no ar"
 });
 
+interface Mamifero {
+    nome: String;
+    caracteristicas: String;
+    peso: number;
+    ameacado: boolean;
+}
+
+server.post<{ Body: Mamifero }>('/inserir', (request, reply) => {
+    const { nome, caracteristicas, peso, ameacado } = request.body;
+
+    const mamifero = prisma.leopardoDasNeves.create({
+        data: {
+            nome,
+            caracteristicas,
+            peso,
+            ameacado,
+        }
+    });
+    return reply.status(201).send(mamifero);
+});
+
 const port: any = process.env.PORT;
 
 server.listen({ port }, (error, adress) => {
-    if(error){
+    if (error) {
         console.error(error);
         process.exit(1);
-    }else{
+    } else {
         console.log(`Servidor rodando em ${adress}`);
     }
 });
