66bb9b1dc7df2333f5af9cb101cc8a8e213850c6
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2023-05-24 02:13:57 -0300

    banco de dados migrado para o postgres com sucesso

---
+++ prisma/migrations/20230524050724_migrando_para_o_banco_de_dados_postgresql/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "leopardoDasNeves" (
+    "nome" TEXT NOT NULL,
+    "caracteristicas" TEXT NOT NULL,
+    "peso" INTEGER NOT NULL,
+    "ameacado" BOOLEAN NOT NULL,
+
+    CONSTRAINT "leopardoDasNeves_pkey" PRIMARY KEY ("nome")
+);
